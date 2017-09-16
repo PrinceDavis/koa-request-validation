6d512e170b0b4650062fbde706b2ece0992625b1
Ossaija ThankGod <codebugsolved@gmail.com>
2017-09-16 07:31:11 +0100

    added functionality for validating request parameters

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const joi = require('joi')
 module.exports = () => {
   return async (ctx, next) => {
     ctx.validateBody = validateBody
+    ctx.validateParams = validateParams
     await next()
   }
 }
 
 function validateBody (ctx, rule) {
   validate(ctx.request.body, rule)
+}
+
+function validateParams (ctx, rule) {
+  validate(ctx.params, rule)
 }
 
 function validate (obj, rule) {
